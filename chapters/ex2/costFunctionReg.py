d950569809f4a95b9806707fc4a7a5507b31fef8
Nils Randau <nilsrandau@gmail.com>
2018-07-05 23:12:05 +0200

    Updated ex2 regularized costFunction

--- chapters/ex2/costFunctionReg.py
+++ chapters/ex2/costFunctionReg.py
@@ -13,19 +13,12 @@ def costFunctionReg(theta, X, y, Lambda):
     # Initialize some useful values
     m = len(y)   # number of training examples
     n = len(theta)
-
-    theta = np.matrix(theta).T
-    X = np.matrix(X)
-    y = np.matrix(y).T
     
-    z = X.dot(theta)
-    J = -1/m * ( np.sum( y*np.log(sigmoid(z)) + (np.ones(m)-y) * np.log(np.ones(m) - sigmoid(z)) ) ) + (Lambda/(2*m)*np.sum(np.array(theta)[1:n]**2))
+    z = X.dot(theta) 
+    J = -1/m * np.sum( y*np.log(sigmoid(z)) + ((np.ones(m)-y) * (np.log(np.ones(m) - sigmoid(z))) ) ) + (Lambda/(2*m)*np.sum(np.array(theta)[1:n]**2))
     
-    error = sigmoid(X * theta) - y
-    grad = np.zeros(n)
-    
-
-    
+    error = sigmoid(X.dot(theta)) - y
+    grad = np.zeros(n) 
     grad = 1/m*(X.T.dot(error)+np.sum(np.array(theta)[1:n]))
         
     return J, grad
@@ -38,12 +31,3 @@ def costFunctionReg(theta, X, y, Lambda):
 
 # =============================================================================
 
-
-def costReg(theta, X, y, learningRate):
-    theta = np.matrix(theta)
-    X = np.matrix(X)
-    y = np.matrix(y)
-    first = np.multiply(-y, np.log(sigmoid(X * theta)))
-    second = np.multiply((1 - y), np.log(1 - sigmoid(X * theta)))
-    reg = (learningRate / 2 * len(X)) * np.sum(np.power(theta[:,1:theta.shape[1]], 2))
-    return np.sum(first - second) / (len(X)) + reg
--- chapters/ex2/test_cases/test_ex2.py
+++ chapters/ex2/test_cases/test_ex2.py
@@ -22,14 +22,15 @@ except(ImportError):
 #     sys.path.append('..')
 # =============================================================================
 import sys, os
-if os.path.abspath(ROOT_DIR+"\\chapters\\ex2") not in sys.path:
-     sys.path.append(ROOT_DIR+"\\chapters\\ex2")
+if os.path.abspath(os.path.join(ROOT_DIR,'chapters','ex2')) not in sys.path:
+     sys.path.append(os.path.join(ROOT_DIR,'chapters','ex2'))
 
 from costFunction import costFunction
+from costFunctionReg import costFunctionReg
 
 class Ex2Test(unittest.TestCase):
     def setUp(self):
-        data = np.loadtxt(ROOT_DIR+'\\chapters\\ex2\\ex2data1.txt', delimiter=',')           
+        data = np.loadtxt(os.path.join(ROOT_DIR,'chapters','ex2','ex2data1.txt'), delimiter=',')           
         m = data.shape[0]
         # Add intercept term to x and X_test        
         data = np.concatenate((np.ones((m, 1)), data), axis=1)  
@@ -38,6 +39,7 @@ class Ex2Test(unittest.TestCase):
         self.X = data[:,0:n-1] 
         self.y = data[:, n-1]
         self.initial_theta = np.zeros(n-1)
+        self.theta = np.array([-34.74982594,   0.28308953,   0.27893718])
          
     def tearDown(self):
         pass
@@ -48,9 +50,26 @@ class Ex2Test(unittest.TestCase):
     def test_X_width(self):
         self.assertEqual(self.X.shape[1], 3)
         
-    def test_cost_function(self): 
+    def test_cost_function0(self): 
         correct = 0.69314718055994529
         estimated = costFunction(self.initial_theta, self.X, self.y)
+        self.assertAlmostEqual(correct, estimated)
+        
+    def test_cost_functionReg_Lambda0(self): 
+        base = costFunction(self.initial_theta, self.X, self.y)
+        regularized = costFunctionReg(self.initial_theta, self.X, self.y,0)[0]
+        self.assertAlmostEqual(base, regularized)
+        
+    def test_cost_functionReg0(self): 
+        Lambda = 0.01
+        correct = 0.6931471805599453
+        estimated = costFunctionReg(self.initial_theta, self.X, self.y, Lambda)[0]
+        self.assertAlmostEqual(correct, estimated)
+    
+    def test_cost_functionRegMin(self): 
+        Lambda = 0.05
+        correct = 0.21380516659878196
+        estimated = costFunctionReg(self.theta, self.X, self.y, Lambda)[0]
         self.assertAlmostEqual(correct, estimated)
  
 if __name__ == '__main__':  
